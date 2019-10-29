8f89f6f4cdf28867b0d90d61cb1a05525336160a
JuliethOsuna <juliosu0324@gmail.com>
2019-10-29 11:57:00 -0500

    Seccion perfil donde se muestran los datos

--- frontend/src/app/myComponent/perfil/perfil.component.ts
+++ frontend/src/app/myComponent/perfil/perfil.component.ts
@@ -6,7 +6,7 @@ import { Component, OnInit } from "@angular/core";
   styleUrls: ["./perfil.component.css"]
 })
 export class PerfilComponent implements OnInit {
-  mostrar = false;
+  mostrar = true;
 
   constructor() {}
 
