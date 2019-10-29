44383634cc5371d21e06e855a3583b7e1ae648f6
deissyMantilla <deissymantilla@outlook.com>
2019-10-29 18:08:51 -0500

    Se agrega el formulario en la seccion de perfil, para actualizar la informacion

--- frontend/src/app/myComponent/perfil/perfil.component.ts
+++ frontend/src/app/myComponent/perfil/perfil.component.ts
@@ -1,14 +1,26 @@
 import { Component, OnInit } from "@angular/core";
+import { FormGroup, FormBuilder, Validators } from "@angular/forms";
+import { ViewEncapsulation } from "@angular/core";
 
 @Component({
   selector: "app-perfil",
   templateUrl: "./perfil.component.html",
-  styleUrls: ["./perfil.component.css"]
+  styleUrls: ["./perfil.component.css"],
+  encapsulation: ViewEncapsulation.None
 })
 export class PerfilComponent implements OnInit {
-  mostrar = true;
+  mostrar = false;
 
-  constructor() {}
+  constructor(private builder: FormBuilder) {}
 
+  actualizarDatosForm: FormGroup = this.builder.group({
+    name: ["", Validators.required],
+    age: [
+      "",
+      Validators.compose([Validators.required, Validators.minLength(1)])
+    ],
+    email: ["", Validators.compose([Validators.required, Validators.email])],
+    password: ["", Validators.required]
+  });
   ngOnInit() {}
 }
